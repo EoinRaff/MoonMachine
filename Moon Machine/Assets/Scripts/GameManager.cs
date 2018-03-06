b78c210e5097c20edba3550e59259b62e11c3d24
EoinRaff <eoinrafferty@gmail.com>
2018-03-06 11:39:46 +0100

    Added materials from GM introduction
    Added Game Manager, Input Manager, organizing folders and Main scene

---
+++ Moon Machine/Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance = null;
+	public GameObject inputManagerPrefab;
+
+	private InputManager inputManager;
+
+	private int currentLevel = 0;
+	private string currentState = "paused";
+
+
+	private void Awake()
+    {
+		if (instance == null)
+		{
+			instance = this;
+		} else if (instance != this)
+		{
+			Destroy(this.gameObject);
+		}
+
+		DontDestroyOnLoad(this.gameObject);
+
+		inputManager = Instantiate(inputManagerPrefab).GetComponent<InputManager>();
+		inputManager.transform.parent = this.transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		CheckInput();
+    }
+
+	public void CheckInput(){
+		inputManager.checkInput();
+	}
+}
