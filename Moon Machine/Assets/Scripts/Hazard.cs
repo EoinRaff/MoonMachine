d1cb77a2b5e3e8bcb0308c07982584037851d475
EoinRaff <eoinrafferty@gmail.com>
2018-03-13 13:20:37 +0100

    Mereged Hazard with Main, added gameManager reference

--- Moon Machine/Assets/Scripts/Hazard.cs
+++ Moon Machine/Assets/Scripts/Hazard.cs
@@ -7,11 +7,19 @@ public class Hazard : MonoBehaviour {
 	//TODO change behaviour for different types of hazard
 
 	public string HazardType;
+	
+	GameManager gameManager;
+
+	private void Awake() {
+		//this should set game manager references
+		gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
+	}
 
 	private void OnTriggerEnter(Collider other) {
 		if (other.tag == "Player")
 		{
 			Debug.Log("Player was killed by " + HazardType);
+			gameManager.ResetLevel();
 		}
 	}
 }
