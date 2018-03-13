e268a0ba382e3abc6391cc2702b6c0ef717c32cd
EoinRaff <eoinrafferty@gmail.com>
2018-03-13 12:56:45 +0100

    Added an ObstacleTest Level
    created hazard class and edited some materials

---
+++ Moon Machine/Assets/Scripts/Hazard.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazard : MonoBehaviour {
+	
+	//TODO change behaviour for different types of hazard
+
+	public string HazardType;
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.tag == "Player")
+		{
+			Debug.Log("Player was killed by " + HazardType);
+		}
+	}
+}
