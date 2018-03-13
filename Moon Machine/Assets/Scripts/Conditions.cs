80c3e3d9d5933c6931ae070d7358b1cd71492bb5
MikkiJordan <rasmus.j.v@hotmail.com>
2018-03-13 12:30:30 +0100

    Conditions
    * When the player reach the goal, the next level is loaded, however this has to be setup in the "build settings"
    
    * When you hit the floor the level is reloaded.

---
+++ Moon Machine/Assets/Scripts/Conditions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Conditions : MonoBehaviour
+{
+	private void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.name == "Player")
+		{
+			Scene scene = SceneManager.GetActiveScene(); // We get the current level that the player is playing
+			if (this.name == "Floor")
+			{
+				SceneManager.LoadScene(scene.name); // We use this because application.loadLevel is obsolete
+			}
+
+			if (this.name == "FinishPlatform")
+			{
+				SceneManager.LoadScene(scene.buildIndex + 1);    // We check what levels are in the build, and take the one after this.
+			}
+		}
+	}
+}
