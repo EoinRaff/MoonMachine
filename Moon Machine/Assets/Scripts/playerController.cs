77d1e64b75f8418b1371b76731163ff7012e9466
EoinRaff <eoinrafferty@gmail.com>
2018-03-20 14:25:31 +0100

    New Player Controls
    Edited Player Controller script and added playerMotor script based on Brakeys FPS tutorial. Added MoonGen. controls to player Controller

--- Moon Machine/Assets/Scripts/playerController.cs
+++ Moon Machine/Assets/Scripts/playerController.cs
@@ -2,42 +2,94 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour {
+[RequireComponent(typeof(playerMotor))]
+public class playerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3f;
 
 
-	public float speed = 10.0f;
-
-	// Use this for initialization
-	void Start () {
-		Cursor.lockState = CursorLockMode.Locked;
-	}
+	public float aimSpeed = 5;
+	public float minDistance = 1f;
+	public GameObject cam;		
 	
-	// Update is called once per frame
-	void Update () {
-        /*if (GetComponentInParent<Attractor>().active)
+	public bool charging;
+
+	[SerializeField]
+	GameObject holoMoonPrefab;
+	GameObject holoMoon;
+
+	[SerializeField]
+	GameObject moonPrefab;
+	GameObject moon;
+
+    private playerMotor motor;
+    void Start()
+    {
+        motor = GetComponent<playerMotor>();
+    }
+
+
+    void Update()
+    {
+		#region Movement and Rotation
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 _movHorizontal = transform.right * xMov;
+        Vector3 _movVertical = transform.forward * zMov;
+
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+
+        motor.Move(_velocity);
+
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+        motor.Rotate(_rotation);
+
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _camerRotation = new Vector3(_xRot, 0f, 0f) * lookSensitivity;
+
+        motor.RotateCamera(_camerRotation);
+		#endregion
+
+		#region MoonControl
+		if (Input.GetMouseButtonDown(0))
 		{
-			return;
+			charging = true;
+			holoMoon = Instantiate(holoMoonPrefab, transform.position + cam.transform.forward * minDistance, cam.transform.rotation);
 		}
-		else
-		{		
-			float translation = Input.GetAxis("Vertical") * speed;
-			float strafe = Input.GetAxis("Horizontal") * speed;
-			translation *= Time.deltaTime;
-			strafe *= Time.deltaTime;
-
-			transform.Translate(strafe, 0, translation);
-		}*/
-
-        float translation = Input.GetAxis("Vertical") * speed;
-        float strafe = Input.GetAxis("Horizontal") * speed;
-        translation *= Time.deltaTime;
-        strafe *= Time.deltaTime;
-
-        transform.Translate(strafe, 0, translation);
-
-        if (Input.GetKeyDown("escape"))
+		if (Input.GetMouseButton(0))
 		{
-			Cursor.lockState = CursorLockMode.None;
+			if (charging)
+			{
+				holoMoon.transform.position += cam.transform.forward * Time.deltaTime * aimSpeed;
+				//holoMoon.GetComponent<Rigidbody>().MovePosition(cam.transform.forward * Time.deltaTime * aimSpeed);
+			}
 		}
-	}
+		if (Input.GetMouseButtonUp(0))
+		{
+			charging = false;
+			Destroy(holoMoon);
+			Destroy(moon);
+			moon = Instantiate(moonPrefab, holoMoon.transform.position, holoMoon.transform.rotation);
+			//moonAtractor = moon.GetComponent<Attractor>();
+			//thisAttractor.active = true;
+			//rb.useGravity = false;
+		}
+		if (Input.GetMouseButton(1))
+		{
+			Destroy(moon);
+			//thisAttractor.active = false;
+			//rb.useGravity = true;
+		}
+		#endregion
+    }
+
 }
