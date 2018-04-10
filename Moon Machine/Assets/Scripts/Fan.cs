5d913f0a93a0e3914035dbf9fa2c860f4cdd33aa
puna47 <puna47@gmail.com>
2018-04-10 16:04:54 +0200

    FanLevelGin
    New level with two fans rotating.

---
+++ Moon Machine/Assets/Scripts/Fan.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fan : MonoBehaviour {
+
+    GameObject fan;
+
+	void Start () {
+        Transform fanOne = fan.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(0, 0, Time.deltaTime*3);
+	}
+}
