1a676d21957bc77b1bc988174fef6273fd8c9fb7
EoinRaff <eoinrafferty@gmail.com>
2018-04-10 16:59:14 +0200

    Why Wont it work across different levels
    oh god I have no idea what is happening

--- Moon Machine/Assets/Scripts/GameManager.cs
+++ Moon Machine/Assets/Scripts/GameManager.cs
@@ -10,9 +10,10 @@ public class GameManager : MonoBehaviour
 
     public static GameManager instance = null;
     public GameObject inputManagerPrefab;
-	public GameObject pauseMenuUI;
+    public GameObject pauseMenuUIPrefab;
 
     private InputManager inputManager;
+	private GameObject pauseMenuUI;
 
     private Scene scene;
     private int currentLevel = 0;
@@ -36,11 +37,36 @@ public class GameManager : MonoBehaviour
         inputManager = Instantiate(inputManagerPrefab).GetComponent<InputManager>();
         inputManager.transform.parent = this.transform;
 
+		if (SceneManager.GetActiveScene().buildIndex > 0)
+		{
+			Canvas canvas = GameObject.FindObjectOfType<Canvas>();
+			pauseMenuUI = Instantiate(pauseMenuUIPrefab);
+			pauseMenuUI.transform.parent = canvas.transform;
+		}
     }
+    private void Start()
+    {
+        try
+        {
+            //pauseMenuUI = GameObject.FindGameObjectWithTag("Pause");
+
+        }
+        catch (System.Exception)
+        {
+            Debug.LogError("Can't find pause screen");
+            throw;
+        }
+    }
+
 
     void Update()
     {
         CheckInput();
+		if (pauseMenuUI == null)
+		{
+			Debug.Log("Look for UI Element");
+			pauseMenuUI = GameObject.FindGameObjectWithTag("Pause");
+		}
     }
 
     public void CheckInput()
@@ -62,17 +88,18 @@ public class GameManager : MonoBehaviour
     public void Pause()
     {
         gameIsPaused = true;
-		pauseMenuUI.SetActive(true);
-		Time.timeScale = 0f;
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.Confined;
 
     }
     public void Resume()
     {
+        //Destroy(pauseMenuUI);
         gameIsPaused = false;
-		pauseMenuUI.SetActive(false);
-		Time.timeScale = 1f;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
     }
