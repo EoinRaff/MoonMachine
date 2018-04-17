6a6632ca9b8e17b9bb3805e42cbb6bdeabc24b4e
EoinRaff <eoinrafferty@gmail.com>
2018-04-17 18:03:28 +0200

    Made Tutorial Level
    Also made framework for moon status UI

---
+++ Moon Machine/Assets/Scripts/HUD.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD : MonoBehaviour {
+
+	public GameObject player;
+	moonGenerator moonGenerator;
+	// Use this for initialization
+	void Start () {
+		moonGenerator = player.GetComponent<moonGenerator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!moonGenerator.canShootMoon)
+		{
+			//display NOT READY
+		}
+		else
+		{
+			if (moonGenerator.charging)
+			{
+				//display CHARGING
+			}
+			else
+			{
+				//display READY
+			}
+		}
+	}
+}
