c3bd84cb9deb6c95208ed73515304e4d8e308998
EoinRaff <eoinrafferty@gmail.com>
2018-04-24 12:14:45 +0200

    added reset script to credits scene

---
+++ Moon Machine/Assets/restart.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class restart : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Debug.Log("esc");
+			SceneManager.LoadScene(0);
+        }
+	}
+}
